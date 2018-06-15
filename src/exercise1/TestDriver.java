cf26b129bf8e2b357462fc60abf22ff47cf9585b
Chien-Hua <39634918+Chien-Hua@users.noreply.github.com>
2018-06-15 17:23:05 -0400

    changed all questions

--- src/exercise1/TestDriver.java
+++ src/exercise1/TestDriver.java
@@ -6,12 +6,12 @@ public class TestDriver {
     public static void main(String[] args) {
         Question[] questions = new Question[5];
         questions[0] = new Question("Java is originally created by what company?","Sun Microsystems");
-        questions[1] = new Question("What is the color for eggpant?","Purple");
-        questions[2] = new Question("What is the color for orange?","Orange");
-        questions[3] = new Question("What is the color for moon?","Yellow");
-        questions[4] = new Question("What is the color for tree?","Green");
+        questions[1] = new Question("What is the collection of related objects?","class");
+        questions[2] = new Question("Objects are created by using the ______ keyword","new");
+        questions[3] = new Question(" _______ variables are declared INSIDE a class but OUTSIDE of method bodies","Instance");
+        questions[4] = new Question("What can be used to compile multiple classes at once?","javac");
 
-        Test color = new Test(questions);
-        color.startTest();
+        Test java = new Test(questions);
+        java.startTest();
     }
 }
