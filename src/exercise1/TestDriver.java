e9eddeedb77f095505dff5164283ebf05555574b
Chien-Hua <39634918+Chien-Hua@users.noreply.github.com>
2018-06-15 16:32:09 -0400

    changed question 1

--- src/exercise1/TestDriver.java
+++ src/exercise1/TestDriver.java
@@ -5,7 +5,7 @@ public class TestDriver {
 
     public static void main(String[] args) {
         Question[] questions = new Question[5];
-        questions[0] = new Question("What is the color for sky?","Blue");
+        questions[0] = new Question("Java is originally created by what company?","Sun Microsystems");
         questions[1] = new Question("What is the color for eggpant?","Purple");
         questions[2] = new Question("What is the color for orange?","Orange");
         questions[3] = new Question("What is the color for moon?","Yellow");
